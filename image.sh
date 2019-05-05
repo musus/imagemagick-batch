6ece8d830dc4d2543763b20fb9e1167ca49f9962
Susumu Seino <seino@susu.mu>
2019-05-05 09:26:24 +0900

    Change function

--- image.sh
+++ image.sh
@@ -1,15 +1,41 @@
 #!/bin/sh
-if [ "$1" = "" ]
-then
-    echo "[ERROR] 引数にリストのファイルを指定してください。"
-    exit
+
+# if [ "$1" = "" ]
+# then
+#     echo "[ERROR] 引数にリストのファイルを指定してください。"
+#     exit
+# fi
+
+
+
+ALLFILE=$(ls ./ | wc -l)
+EXCLUSION=1
+
+if [ ! -d "resize" ]; then
+	mkdir "resize"
+else
+	let EXCLUSION++
 fi
 
-for i in $(cat $1)
+CONVERTFILE=$(( $ALLFILE - $EXCLUSION))
+
+echo "Converting " $CONVERTFILE "files"
+
+COUNTER=0
+
+for file in *.jpg *.JPG *.jpeg *.JPEG;
 do
-    echo "---------------------"
-    echo "Target File: ${i}"
-    filename=${i%.jpg}
-    echo "${filename}";
-    convert -resize 1200x -quality 85 "${filename}.jpg" "${filename}.jpg"
+    [ -f "$file" ] || continue
+    # COUNTER=$((counter++))
+    let COUNTER++
+	echo "convert" $file "-" $COUNTER "/" $CONVERTFILE;
+    convert -resize 1200x\> -quality 80 "$file" "resize/$file"
+done
+
+for file in *.png;
+do
+	let COUNTER++
+	echo "convert" $file "-" $COUNTER "/" $CONVERTFILE;
+    [ -f "$file" ] || continue
+	convert -resize 1200x\> -quality 80 "$file" "resize/$file"
 done
