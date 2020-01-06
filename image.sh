763d27a5a3b356fa1701e9cdfe92ccbf4bdf9cb0
Susumu Seino <seino@susu.mu>
2020-01-06 15:42:51 +0900

    some fix

--- image.sh
+++ image.sh
@@ -10,6 +10,9 @@ count() {
             if [[ "$1"  = *.jpg  ]]; then
                 let ALLFILE++
             fi
+            if [[ "$1"  = *.jpeg  ]]; then
+                let ALLFILE++
+            fi
             if [[ "$1"  = *.png  ]]; then
                 let ALLFILE++
             fi
@@ -25,10 +28,20 @@ COUNTER=0
 
 imagemin() {
     while [ "$1" ]; do
-        if [ -d "$1" ]; then
+        if [[ -d "$1" ]]; then
             imagemin "$1"/*
         else
-            if [ "$1"  = *.jpg -o "$1" *.png ]; then
+            if [[ "$1"  = *.jpg ]]; then
+                let COUNTER++
+                echo "convert" $1 "-" $COUNTER "/" $ALLFILE;
+                convert -resize 1200x\> -quality 80 "$1" "$1"
+            fi
+            if [[ "$1"  = *.jpeg ]]; then
+                let COUNTER++
+                echo "convert" $1 "-" $COUNTER "/" $ALLFILE;
+                convert -resize 1200x\> -quality 80 "$1" "$1"
+            fi
+            if [[ "$1"  = *.png ]]; then
                 let COUNTER++
                 echo "convert" $1 "-" $COUNTER "/" $ALLFILE;
                 convert -resize 1200x\> -quality 80 "$1" "$1"
